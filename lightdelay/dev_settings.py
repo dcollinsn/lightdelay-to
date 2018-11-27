243ad34a1664249f599422131d815cf03e2f7ad6
Dan Collins <dcollinsn@gmail.com>
2018-11-26 21:02:48 -0500

    Didn't need a database after all

--- lightdelay/dev_settings.py
+++ lightdelay/dev_settings.py
@@ -4,16 +4,3 @@ ALLOWED_HOSTS = [
     '1bp045cgwc.execute-api.us-west-2.amazonaws.com',
     'dev.lightdelay.to',
 ]
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'lightdelay',
-#        'USER': 'lightdelay',
-#        'PASSWORD': 'lightdelay',
-#        'HOST': 'lightdelay.cluster-cpadbrpppxlf.us-west-2.rds.amazonaws.com',
-#        'OPTIONS': {
-#            'init_command': 'SET foreign_key_checks=0;',
-#        },
-#    }
-#}
