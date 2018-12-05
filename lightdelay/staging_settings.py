5f60b720de3dc4e86e2f8748dd5e6f99f50e38fa
Dan Collins <dcollinsn@gmail.com>
2018-12-05 16:59:45 -0500

    Add Staging stage

---
+++ lightdelay/staging_settings.py
@@ -0,0 +1,8 @@
+from lightdelay.dev_settings import *
+
+SECRET_KEY = '1234asdfqwersdfhgbw5fvufwh4euuaew3478gqc'
+DEBUG = False
+ALLOWED_HOSTS = [
+    'xve20vgtn4.execute-api.us-west-2.amazonaws.com',
+    'staging.lightdelay.to',
+]
