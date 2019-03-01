325e1f03fe13642d581436fc63c99f641e52ce67
Dan Collins <dcollinsn@gmail.com>
2019-02-28 20:43:02 -0500

    S3 Storage?

--- lightdelay/dev_settings.py
+++ lightdelay/dev_settings.py
@@ -6,3 +6,14 @@ ALLOWED_HOSTS = [
     'itkucjh4u8.execute-api.us-west-2.amazonaws.com',
     'dev.lightdelay.to',
 ]
+
+INSTALLED_APPS = INSTALLED_APPS + [
+    'django_s3_storage',
+]
+
+STATIC_ROOT = '/tmp/static'
+SASS_PROCESSOR_ROOT = '/tmp/static'
+
+STATICFILES_STORAGE = "django_s3_storage.storage.StaticS3Storage"
+AWS_S3_BUCKET_NAME_STATIC = "dev.static.lightdelay.to"
+AWS_S3_PUBLIC_URL_STATIC = "https://dev.static.lightdelay.to/"
--- lightdelay/staging_settings.py
+++ lightdelay/staging_settings.py
@@ -6,3 +6,6 @@ ALLOWED_HOSTS = [
     'xve20vgtn4.execute-api.us-west-2.amazonaws.com',
     'staging.lightdelay.to',
 ]
+
+AWS_S3_BUCKET_NAME_STATIC = "staging.static.lightdelay.to"
+AWS_S3_PUBLIC_URL_STATIC = "https://staging.static.lightdelay.to/"
